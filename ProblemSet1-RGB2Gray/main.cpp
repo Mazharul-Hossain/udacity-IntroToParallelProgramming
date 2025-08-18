adf9f6c6249e847dcadf90b6dbcdd4c8b7b3cfd7
Mazharul Hossain <mhossain9@memphis.edu>
2025-08-17 20:14:39 -0500

    ProblemSet1-RGB2Gray

--- ProblemSet1-RGB2Gray/main.cpp
+++ ProblemSet1-RGB2Gray/main.cpp
@@ -1,4 +1,4 @@
-//Udacity HW1 Solution
+// Udacity HW1 Solution
 
 #include <iostream>
 #include "timer.h"
@@ -8,86 +8,89 @@
 #include "reference_calc.h"
 #include "compare.h"
 
-void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, 
-                            uchar4 * const d_rgbaImage,
-                            unsigned char* const d_greyImage, 
+void your_rgba_to_greyscale(const uchar4 *const h_rgbaImage,
+                            uchar4 *const d_rgbaImage,
+                            unsigned char *const d_greyImage,
                             size_t numRows, size_t numCols);
 
-//include the definitions of the above functions for this homework
+// include the definitions of the above functions for this homework
 #include "HW1.cpp"
 
-int main(int argc, char **argv) {
-  uchar4        *h_rgbaImage, *d_rgbaImage;
-  unsigned char *h_greyImage, *d_greyImage;
+int main(int argc, char **argv)
+{
+    uchar4 *h_rgbaImage, *d_rgbaImage;
+    unsigned char *h_greyImage, *d_greyImage;
 
-  std::string input_file;
-  std::string output_file;
-  std::string reference_file;
-  double perPixelError = 0.0;
-  double globalError   = 0.0;
-  bool useEpsCheck = false;
-  switch (argc)
-  {
-	case 2:
-	  input_file = std::string(argv[1]);
-	  output_file = "HW1_output.png";
-	  reference_file = "HW1_reference.png";
-	  break;
-	case 3:
-	  input_file  = std::string(argv[1]);
-      output_file = std::string(argv[2]);
-	  reference_file = "HW1_reference.png";
-	  break;
-	case 4:
-	  input_file  = std::string(argv[1]);
-      output_file = std::string(argv[2]);
-	  reference_file = std::string(argv[3]);
-	  break;
-	case 6:
-	  useEpsCheck=true;
-	  input_file  = std::string(argv[1]);
-	  output_file = std::string(argv[2]);
-	  reference_file = std::string(argv[3]);
-	  perPixelError = atof(argv[4]);
-      globalError   = atof(argv[5]);
-	  break;
-	default:
-      std::cerr << "Usage: ./HW1 input_file [output_filename] [reference_filename] [perPixelError] [globalError]" << std::endl;
-      exit(1);
-  }
-  //load the image and give us our input and output pointers
-  preProcess(&h_rgbaImage, &h_greyImage, &d_rgbaImage, &d_greyImage, input_file);
+    std::string input_file = "cinque_terre_gray.jpg";
+    std::string output_file = "HW1_output.png";
+    std::string reference_file = "HW1_reference.png";
+    double perPixelError = 0.0;
+    double globalError = 0.0;
+    bool useEpsCheck = false;
+    switch (argc)
+    {
+    case 2:
+        input_file = std::string(argv[1]);
+        break;
+    case 3:
+        input_file = std::string(argv[1]);
+        output_file = std::string(argv[2]);
+        break;
+    case 4:
+        input_file = std::string(argv[1]);
+        output_file = std::string(argv[2]);
+        reference_file = std::string(argv[3]);
+        break;
+    case 6:
+        useEpsCheck = true;
+        input_file = std::string(argv[1]);
+        output_file = std::string(argv[2]);
+        reference_file = std::string(argv[3]);
+        perPixelError = atof(argv[4]);
+        globalError = atof(argv[5]);
+        break;
+    default:
+        std::cerr << "Usage: ./HW1 input_file [output_filename] [reference_filename] [perPixelError] [globalError]" << std::endl;
+        exit(1);
+    }
+    
+    printf("Start main: %s\n", input_file.c_str());
 
-  GpuTimer timer;
-  timer.Start();
-  //call the students' code
-  your_rgba_to_greyscale(h_rgbaImage, d_rgbaImage, d_greyImage, numRows(), numCols());
-  timer.Stop();
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // load the image and give us our input and output pointers
+    preProcess(&h_rgbaImage, &h_greyImage, &d_rgbaImage, &d_greyImage, input_file);
 
-  int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
+    GpuTimer timer;
+    timer.Start();
+    // call the students' code
+    your_rgba_to_greyscale(h_rgbaImage, d_rgbaImage, d_greyImage, numRows(), numCols());
+    timer.Stop();
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
 
-  if (err < 0) {
-    //Couldn't print! Probably the student closed stdout - bad news
-    std::cerr << "Couldn't print timing information! STDOUT Closed!" << std::endl;
-    exit(1);
-  }
+    int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
 
-  size_t numPixels = numRows()*numCols();
-  checkCudaErrors(cudaMemcpy(h_greyImage, d_greyImage, sizeof(unsigned char) * numPixels, cudaMemcpyDeviceToHost));
+    if (err < 0)
+    {
+        // Couldn't print! Probably the student closed stdout - bad news
+        std::cerr << "Couldn't print timing information! STDOUT Closed!" << std::endl;
+        exit(1);
+    }
 
-  //check results and output the grey image
-  postProcess(output_file, h_greyImage);
+    size_t numPixels = numRows() * numCols();
+    checkCudaErrors(cudaMemcpy(h_greyImage, d_greyImage, sizeof(unsigned char) * numPixels, cudaMemcpyDeviceToHost));
 
-  referenceCalculation(h_rgbaImage, h_greyImage, numRows(), numCols());
+    // check results and output the grey image
+    postProcess(output_file, h_greyImage);
 
-  postProcess(reference_file, h_greyImage);
+    referenceCalculation(h_rgbaImage, h_greyImage, numRows(), numCols());
 
-  //generateReferenceImage(input_file, reference_file);
-  compareImages(reference_file, output_file, useEpsCheck, perPixelError, 
-                globalError);
+    postProcess(reference_file, h_greyImage);
 
-  cleanup();
+    // generateReferenceImage(input_file, reference_file);
+    compareImages(reference_file, output_file, useEpsCheck, perPixelError,
+                  globalError);
 
-  return 0;
+    cleanup();
+
+    return 0;
 }
